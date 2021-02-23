f891fd63cb5a5667dd7ef66a6649747a88f8c6f8
varseienko <varseienko@salesforce.com>
2021-02-22 20:22:32 -0800

    feat: rollup config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+export default [
+
+    {
+        input: './packages/@nucleus-integration/kiwi/src/index.js',
+        output: {
+            file: './build/@nucleus-integration/kiwi/index.js',
+            format: 'iife',
+        }
+    },
+    {
+        input: './packages/@nucleus-integration/lemon/src/index.js',
+        output: {
+            file: './build/@nucleus-integration/lemon/index.js',
+            format: 'iife',
+        }
+    },
+    {
+        input: './packages/nucleus-integration-banana/src/index.js',
+        output: {
+            file: './build/@nucleus-integration-banana/index.js',
+            format: 'iife',
+        }
+    },
+]
